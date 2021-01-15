634f1cfd07816bbef3f38db5d23349c4ac54aef1
VyshnavVk <vyshnavvyshu20@gmail.com>
2021-01-15 16:09:06 +0530

    database backup function added

--- app/src/main/java/com/sangsolutions/powerbear/Database/DatabaseHelper.java
+++ app/src/main/java/com/sangsolutions/powerbear/Database/DatabaseHelper.java
@@ -12,9 +12,13 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -345,6 +349,52 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
 //Tools for sqlite exicution
+
+    public static String copyDatabase(Context c) {
+        String databasePath = c.getDatabasePath(DATABASE_NAME).getPath();
+        File f = new File(databasePath);
+        OutputStream myOutput = null;
+        InputStream myInput = null;
+        File directory = null;
+        Log.d("testing", " testing db path " + databasePath);
+        Log.d("testing", " testing db exist " + f.exists());
+
+        if (f.exists()) {
+            try {
+
+                 directory = new File(c.getExternalFilesDir(null) + File.separator +"database");
+                if (!directory.exists())
+                    directory.mkdir();
+
+                myOutput = new FileOutputStream(directory.getAbsolutePath()
+                        + "/" + DATABASE_NAME);
+                myInput = new FileInputStream(databasePath);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = myInput.read(buffer)) > 0) {
+                    myOutput.write(buffer, 0, length);
+                }
+
+                myOutput.flush();
+
+            } catch (Exception e) {
+            } finally {
+                try {
+                    if (myOutput != null) {
+                        myOutput.close();
+                        myOutput = null;
+                    }
+                    if (myInput != null) {
+                        myInput.close();
+                        myInput = null;
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }
+        return directory.getAbsolutePath()+ "/" +DATABASE_NAME;
+    }
 
     private void executeSQLScript(SQLiteDatabase db, BufferedReader reader) throws IOException {
         String line;
--- app/src/main/java/com/sangsolutions/powerbear/Home.java
+++ app/src/main/java/com/sangsolutions/powerbear/Home.java
@@ -1,8 +1,12 @@
 package com.sangsolutions.powerbear;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,8 +18,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -33,10 +39,11 @@ import java.util.List;
 public class Home extends AppCompatActivity {
 LinearLayout sync_ll, delivery_ll, stock_count_ll, goods_ll, report_ll, grn_without_po_ll;
 DatabaseHelper helper;
-ImageView img_logout;
+ImageView img_logout,img_backup;
 TextView tv_username,tv_date;
 ImageView img_settings;
     SharedPreferences preferences;
+    String[] PERMISSIONS = {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
     @Override
     protected void onResume() {
         super.onResume();
@@ -45,6 +52,36 @@ ImageView img_settings;
         tv_date.setText(df.format(c));
     }
 
+    //permission method
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    /////////////////////////////////////////
+
+    public void BackUpAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Back Up!")
+                .setMessage("Do you want to backup the database file?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Home.this, "file copied to :"+helper.copyDatabase(Home.this), Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
+    }
 
     @Override
     public void onBackPressed() {
@@ -63,6 +100,7 @@ ImageView img_settings;
         report_ll = findViewById(R.id.report);
         img_logout = findViewById(R.id.logout);
         tv_username = findViewById(R.id.username);
+        img_backup = findViewById(R.id.backup);
         tv_date = findViewById(R.id.date);
         grn_without_po_ll = findViewById(R.id.goods_without_po);
         img_settings = findViewById(R.id.settings);
@@ -246,6 +284,18 @@ ImageView img_settings;
                 }
             }
         });
+    img_backup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(!hasPermissions(Home.this, PERMISSIONS)){
+                ActivityCompat.requestPermissions(Home.this, PERMISSIONS, 100);
+            }else {
+                BackUpAlert();
+            }
+
+        }
+    });
+
     }
 
 
